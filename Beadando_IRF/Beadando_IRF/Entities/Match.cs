ce0e2c7ce92884406b72b13a572acdc83cd290a9
BXZJIT <peter.bezzeg@stud.uni-corvinus.hu>
2020-12-06 19:05:58 +0100

    Highlight related matches

---
+++ Beadando_IRF/Beadando_IRF/Entities/Match.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beadando_IRF.Entities
+{
+   
+       public class Match
+        {
+            public string id { get; set; }
+            public string localTeam { get; set; }
+            public string visitorTeam { get; set; }
+            public string localAndVisitor { get; set; }
+        }
+    
+}
--- Beadando_IRF/Beadando_IRF/Fogadas_Form.cs
+++ Beadando_IRF/Beadando_IRF/Fogadas_Form.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Beadando_IRF.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,7 @@ namespace Beadando_IRF
     public partial class Fogadas_Form : Form
     {
         List <string> championships = new List<string>();
+        List<Match> matches = new List<Match>();
         XmlDocument doc;
         public Fogadas_Form()
         {
@@ -47,7 +49,55 @@ namespace Beadando_IRF
 
         private void Bajnoksag_Listbox_SelectedIndexChanged(object sender, EventArgs e)
         {
-            Console.WriteLine(Bajnoksag_Listbox.SelectedItem);
+            matches.Clear();
+            string selectedChampionship = Bajnoksag_Listbox.SelectedItem.ToString();
+
+            // Kategóriák
+            foreach (XmlElement element in doc.DocumentElement)
+            {
+                if (element.GetAttribute("name") == selectedChampionship)
+                {
+                    // Meccsek
+                    foreach (XmlElement match in element.FirstChild)
+                    {
+                        Match newMatch = new Match();
+                        newMatch.id = match.GetAttribute("id");
+                        //Console.WriteLine("NewMatchId: " + match.GetAttribute("id"));
+                        string localTeam = "";
+                        string visitorTeam = "";
+
+                        // Meccs gyermekeiből hazai és vendég csapat tárolása
+                        foreach (XmlElement matchChild in match.ChildNodes)
+                        {
+                            if (matchChild.Name == "localteam")
+                            {
+                                localTeam = matchChild.GetAttribute("name");
+                                newMatch.localTeam = localTeam;
+                                //Console.WriteLine("Localteam: " + matchChild.GetAttribute("name"));
+                            }
+
+                            if (matchChild.Name == "visitorteam")
+                            {
+                                visitorTeam = matchChild.GetAttribute("name");
+                                newMatch.visitorTeam = visitorTeam;
+                                //Console.WriteLine("Visitorteam: " + matchChild.GetAttribute("name"));
+                            }
+                        }
+
+                        // Meccs hozzáadása listához
+                        newMatch.localAndVisitor = localTeam + "-" + visitorTeam;
+                        matches.Add(newMatch);
+                        //Console.WriteLine("LocalAndVisitor: " + localTeam + "-" + visitorTeam);
+                    }
+                }
+            }
+            // Listbox frissítése
+            Merkozes_Listbox.DataSource = new List<string>();
+            Merkozes_Listbox.DataSource = matches;
+            Merkozes_Listbox.DisplayMember = "localAndVisitor";
+            Merkozes_Listbox.ValueMember = "id";
+
+
 
         }
     }
